0f4da5780e6a0ba1e888b903bd5d015a900eb88f
Thi Pham <thi.pham@mygeorgian.ca>
2021-08-16 10:40:00 -0400

    use urllib instead of requests

--- data_accquiring.py
+++ data_accquiring.py
@@ -4,28 +4,23 @@ from pymongo.errors import BulkWriteError
 from requests import exceptions
 from conf import USER_AGENT, ROOT_URL
 from db_utilities import DbUtiltity
-#import urllib.request
-import requests
+import urllib.request
 import re
 
 
 def start_data_accquiring():
-    # headers = {'User-Agent':USER_AGENT}
-    #request = urllib.request.Request(ROOT_URL,None,headers) #The assembled request
-    
-    #response = urllib.request.urlopen(request)
-    
-
-    
+    headers = {'User-Agent':USER_AGENT}
 
     # find total number of cars
     total_number_of_cars = 0
     n_tries = 0
     max_tries = 5
     while total_number_of_cars == 0 and n_tries < max_tries:
+        n_tries += 1
         try:
-            response = requests.get(ROOT_URL)
-            content = response.text
+            request = urllib.request.Request(ROOT_URL,None,headers) #The assembled request
+            response = urllib.request.urlopen(request)
+            content = response.read().decode()
             page_content = BeautifulSoup(content, 'html.parser')
 
             span_tag = page_content.find('span', attrs={"data-qa":"pagination-text"})
@@ -33,7 +28,7 @@ def start_data_accquiring():
             result = pattern.search(span_tag.get_text())
             total_in_str = result.group(3).strip()
             total_number_of_cars = int(total_in_str)
-            n_tries += 1
+            
         except Exception as e:
             print(e)
 
@@ -68,9 +63,9 @@ def process_one_page(current_page):
 
     url_str = f'{ROOT_URL}?page={current_page}'
     headers = {'User-Agent':USER_AGENT}
-    response = requests.get(url_str)
-    #response = urllib.request.urlopen(request)
-    content = response.text
+    request = urllib.request.Request(url_str,None,headers) #The assembled request
+    response = urllib.request.urlopen(request)
+    content = response.read().decode()
 
     page_content = BeautifulSoup(content, 'html.parser')
 
